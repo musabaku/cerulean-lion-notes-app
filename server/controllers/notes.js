0adc83cba3ca7b094018839976341d1b2223be1d
MukuFlash03 <mukulm2010@gmail.com>
2023-07-31 09:26:52 +0530

    Added Delete note endpoint.

--- server/controllers/notes.js
+++ server/controllers/notes.js
@@ -3,6 +3,7 @@ import express from "express";
 
 const router = express.Router();
 
+// Get all notes
 export const getNotes = router.get("/notes", (req, res) => {
 	Note.find({})
 		.then((notes) => {
@@ -14,7 +15,24 @@ export const getNotes = router.get("/notes", (req, res) => {
 		});
 });
 
-// TODO: Add POST route to create a new note
+// Get a note by ID
+export const getNoteById = router.get("/notes/:id", (req, res) => {
+	const id = req.params.id;
+
+	Note.findById(id)
+		.then((note) => {
+			if (!note) {
+				return res.status(404).json({ message: "Note not found" });
+			}
+			res.status(200).json(note);
+		})
+		.catch((err) => {
+			console.error("Error getting notes:", err);
+			return res.status(500).json({ message: "Internal Server Error" });
+		});
+});
+
+// Create a new note
 export const createNote = router.post("/notes", (req, res) => {
 	const { title, content } = req.body;
 	const note = new Note({ title, content });
@@ -27,6 +45,25 @@ export const createNote = router.post("/notes", (req, res) => {
 			console.error("Error creating note:", err);
 			return res.status(500).json({ message: "Internal Server Error" });
 		});
-})
+});
+
 // TODO: Add PUT route to update a note
-// TODO: Add DELETE route to delete a note
+
+// Delete a note
+export const deleteNote = router.delete("/notes/:id", async (req, res) => {
+	const { id } = req.params;
+
+	Note.findByIdAndDelete(id)
+		.then((note) => {
+			if (!note) {
+				return res.status(404).json({ message: "Note not found" });
+			}
+			res.status(200).json({
+				message: `Note title: '${note.title}' deleted successfully`,
+			});
+		})
+		.catch((err) => {
+			console.error("Error deleting note:", err);
+			return res.status(500).json({ message: "Internal Server Error" });
+		});
+});
--- server/routes/notes.js
+++ server/routes/notes.js
@@ -3,12 +3,11 @@ import * as NotesController from "../controllers/notes.js";
 
 const router = express.Router();
 
-// TODO: Add a route to get all notes
 router.get("/notes", NotesController.getNotes);
-
-// TODO: Add a route to create a new note
+router.get("/notes/:id", NotesController.getNoteById);
 router.post("/notes", NotesController.createNote);
+router.delete("/notes/:id", NotesController.deleteNote);
+
 // TODO: Add a route to update a note
-// TODO: Add a route to delete a note
 
 export default router;
